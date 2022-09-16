dcf214c5838d63783d79f9a895054d44b6b73b97
swathigasbuddy <sponnada@gasbuddy.com>
2022-09-16 18:39:37 +0530

    bare react native typescript  app

--- App.tsx
+++ App.tsx
@@ -70,25 +70,8 @@ const App: () => Node = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Home</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -96,21 +79,14 @@ const App: () => Node = () => {
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  titleContainer: {
+    backgroundColor: 'white',
   },
-  sectionTitle: {
+  title: {
     fontSize: 24,
     fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    alignSelf: 'center',
+    marginTop: 10,
   },
 });
 
