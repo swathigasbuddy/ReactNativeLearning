a4924d16bb57870ecda583794422f5167e201a57
swathigasbuddy <sponnada@gasbuddy.com>
2022-09-16 20:10:48 +0530

    bottom tab navigator set up

---
+++ Screens/Screen2.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const Screen2 = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  <SafeAreaView style={backgroundStyle}>
+    <StatusBar
+      barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+      backgroundColor={backgroundStyle.backgroundColor}
+    />
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      style={backgroundStyle}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Screen 2</Text>
+      </View>
+    </ScrollView>
+  </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+});
+export default Screen2;
